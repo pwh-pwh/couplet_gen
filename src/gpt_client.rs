f8a4310d24b546c0e5325bea5528f2ca6124c64b
pwh-pwh <plmoknpwh@163.com>
2025-01-24 16:47:40 +0800

    unimpl

--- src/gpt_client.rs
+++ src/gpt_client.rs
@@ -4,14 +4,16 @@ use serde_json::json;
 pub fn gen_couplet_by_gpt(theme:&str){
     let api_key = "sk-r0VilKsVIiBnCcWJaH18GdsXQQwO5ez6Jl8w2MrdXv7IQWc3";
     let url = "https://api.chatanywhere.tech/v1/chat/completions";
-    let content = "请根据我的提示生成一组春联，包含上联、下联各一句，和横批，其中上联、下联各7个字，横批4个字，\
-    春联主题为：{}\
-    以json格式返回，json字段如下：\
-    {{\
-        \"title\": \"春风得意百花开\",\
-        \"top\": \"万事顺遂福满门\",\
-        \"bottom\": \"新春快乐\"\
-    }}";
+    let template = "请根据我的提示生成一组春联，包含上联、下联各一句，和横批，其中上联、下联各7个字，横批4个字，\
+    春联主题为：{theme};\\n
+直接给出可以被 JSON.parse 解析的字符串，不需要解释内容
+     以json格式返回，返回示例如下：：\
+    {\
+        \"横批\": \"新春快乐\",\
+        \"上联\": \"万事顺遂福满门\",\
+        \"下联\": \"春风得意百花开\"\
+    }";
+    let content = template.replace("{theme}", theme);
     let response: serde_json::Value = ureq::post(url)
         .set("Authorization", &format!("Bearer {}", api_key))
         .set("Content-Type", "application/json")
@@ -34,6 +36,19 @@ mod tests {
 
     #[test]
     fn test_gpt() {
-        gen_couplet_by_gpt("rust技术");
+        gen_couplet_by_gpt("发财");
+    }
+
+    #[test]
+    fn test() {
+        let content = "请根据我的提示生成一组春联，包含上联、下联各一句，和横批，其中上联、下联各7个字，横批4个字，\
+    春联主题为：{}\
+     以json格式返回，返回示例如下：\
+    {\
+        \"title\": \"春风得意百花开\",\
+        \"top\": \"万事顺遂福满门\",\
+        \"bottom\": \"新春快乐\"\
+    }";
+        println!("{}", content);
     }
 }
