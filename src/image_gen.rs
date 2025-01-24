b2cf5e43ec3d5918c67cc3b3aad96cfafd7b79cd
pwh-pwh <plmoknpwh@163.com>
2025-01-24 15:48:42 +0800

    code clean

--- src/image_gen.rs
+++ src/image_gen.rs
@@ -16,7 +16,11 @@ impl Couplet {
 }
 
 pub fn gen_couplet(couplet: &Couplet) {
-    let mut image = ImageReader::open("r1.png").unwrap().decode().unwrap().to_rgba8();
+    let mut image = ImageReader::open("r1.png")
+        .unwrap()
+        .decode()
+        .unwrap()
+        .to_rgba8();
 
     // 使用 imageproc 填充整个图像为白色
     // let white = Rgb([255, 255, 255]);
@@ -24,15 +28,36 @@ pub fn gen_couplet(couplet: &Couplet) {
     let font = FontRef::try_from_slice(include_bytes!("../STXINGKA.TTF")).unwrap();
 
     let size = 80.0;
-    let scale = PxScale {
-        x: size,
-        y: size,
-    };
+    let scale = PxScale { x: size, y: size };
 
     let text = "Rust无敌";
-    draw_text_mut(&mut image, Rgba([0u8, 0u8, 0u8,200u8]), 200, 85, scale, &font, &couplet.title);
-    draw_text_mut_vertical(&mut image, Rgba([0u8, 0u8, 0u8,200u8]), 90, 300, scale, &font, &couplet.bottom);
-    draw_text_mut_vertical(&mut image, Rgba([0u8, 0u8, 0u8,200u8]), 535, 300, scale, &font, &couplet.top);
+    draw_text_mut(
+        &mut image,
+        Rgba([0u8, 0u8, 0u8, 200u8]),
+        200,
+        85,
+        scale,
+        &font,
+        &couplet.title,
+    );
+    draw_text_mut_vertical(
+        &mut image,
+        Rgba([0u8, 0u8, 0u8, 200u8]),
+        90,
+        300,
+        scale,
+        &font,
+        &couplet.bottom,
+    );
+    draw_text_mut_vertical(
+        &mut image,
+        Rgba([0u8, 0u8, 0u8, 200u8]),
+        535,
+        300,
+        scale,
+        &font,
+        &couplet.top,
+    );
     let (w, h) = text_size(scale, &font, text);
     println!("Text size: {}x{}", w, h);
     image.save("result.png").unwrap();
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 mod image_gen;
 
-use ab_glyph::{Font, FontRef, PxScale};
-use image::{ImageReader, Pixel, Rgba};
-use imageproc::definitions::Clamp;
-use imageproc::drawing::{draw_text_mut, text_size, Canvas};
 use crate::image_gen::{gen_couplet, Couplet};
+use ab_glyph::Font;
+use image::Pixel;
+use imageproc::definitions::Clamp;
+use imageproc::drawing::Canvas;
 
 fn main() {
-    gen_couplet(&Couplet::new("Rust无敌".to_string(), "并发性能铸匠魂".to_string(), "安全高效为本心".to_string()));
+    gen_couplet(&Couplet::new(
+        "Rust无敌".to_string(),
+        "并发性能铸匠魂".to_string(),
+        "安全高效为本心".to_string(),
+    ));
 }
